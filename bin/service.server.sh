fa3a583bb01edb7e42c35935ae2b59c3934a81a9
Boris Rybalkin <ribalkin@gmail.com>
2022-03-19 17:31:31 +0000

    sockey

--- bin/service.server.sh
+++ bin/service.server.sh
@@ -2,5 +2,8 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
+export JELLYFIN_kestrel__socket=true
+export JELLYFIN_kestrel__socketPath=/var/snap/jellyfin/current/socket
+
 $DIR/jellyfin/jellyfin --datadir /var/snap/jellyfin/current/data --cachedir /var/snap/jellyfin/current/cache --ffmpeg /snap/jellyfin/current/app/usr/lib/jellyfin-ffmpeg/ffmpeg
 fmpeg
