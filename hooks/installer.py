b0b592442c65a05dba103b690e72d20d010f053c
Boris Rybalkin <ribalkin@gmail.com>
2022-03-20 14:16:25 +0000

    confif

--- hooks/installer.py
+++ hooks/installer.py
@@ -50,7 +50,9 @@ class Installer:
         fs.makepath(join(self.data_dir, 'data', 'plugins'))
         fs.makepath(join(self.data_dir, 'cache'))
         fs.makepath(join(self.data_dir, 'config'))
-        
+        shutil.copy(join(self.snap_dir, 'config', 'system.xml'), join(self.data_dir, 'config', 'system.xml'))
+        shutil.copytree(join(self.snap_dir, 'app', 'plugins', 'LDAP-Auth'), join(self.data_dir, 'data', 'plugins', 'LDAP-Auth'))
+
         fs.chownpath(self.data_dir, USER_NAME, recursive=True)
         fs.chownpath(self.common_dir, USER_NAME, recursive=True)
 
@@ -68,10 +70,7 @@ class Installer:
 
     def _install(self):
         self.log.info('configure install') 
-        shutil.copy(join(self.snap_dir, 'config', 'system.xml'), join(self.data_dir, 'data', 'config', 'system.xml'))
-        shutil.copytree(join(self.snap_dir, 'app', 'plugins', 'LDAP-Auth'), join(self.data_dir, 'data', 'plugins', 'LDAP-Auth'))
-
-        app_storage_dir = storage.init_storage(APP_NAME, USER_NAME)
+               app_storage_dir = storage.init_storage(APP_NAME, USER_NAME)
         with open(self.install_file, 'w') as f:
             f.write('installed\n')
 
