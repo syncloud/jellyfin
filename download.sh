49415a45aaccd9dcc750885f811b47515f0d91cd
Boris Rybalkin <ribalkin@gmail.com>
2022-03-21 21:21:45 +0000

    try ldap v12

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ cd ${DIR}
 
 ARCH=$(uname -m)
 DOWNLOAD_URL=https://github.com/syncloud/3rdparty/releases/download/
-LDAP_VERSION=14.0.0.0
+LDAP_VERSION=12.0.0.0
 
 BUILD_DIR=${DIR}/build/snap
 mkdir -p $BUILD_DIR
