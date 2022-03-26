36f51c4e0631e02515e9dd4d8088d85053a3c345
Boris Rybalkin <ribalkin@gmail.com>
2022-03-26 14:38:32 +0000

    fix ffmpeg

--- bin/ffmpeg.sh
+++ bin/ffmpeg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../app && pwd )
 LIBS=$(echo ${DIR}/usr/lib/*-linux-gnu*)
+LIBS="$LIBS:$(echo ${DIR}/usr/lib/jellyfin-ffmpeg)/lib"
 ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/lib/jellyfin-ffmpeg/ffmpeg "$@"
 "$@"
