e8a6c25551c0e8bcacc85221d66ab33f00a88e6f
Boris Rybalkin <ribalkin@gmail.com>
2022-03-27 15:00:28 +0100

    wait for login

--- integration/lib.py
+++ integration/lib.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 def login(selenium, device_user, device_password):
     selenium.open_app()
     selenium.screenshot('index')
+    selenium.wait_or_screenshot(EC.element_to_be_clickable((By.CSS_SELECTOR, "#txtManualName")))
     selenium.find_by_id("txtManualName").send_keys(device_user)
     password = selenium.find_by_id("txtManualPassword")
     password.send_keys(device_password)
