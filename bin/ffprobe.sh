6a124ea51d1ea19ee814f692a0fba37299d6297f
Boris Rybalkin <ribalkin@gmail.com>
2022-04-23 22:08:56 +0100

    fix lib

--- bin/ffprobe.sh
+++ bin/ffprobe.sh
@@ -2,4 +2,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../app && pwd )
 LIBS=$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS="$LIBS:$(echo ${DIR}/usr/lib/jellyfin-ffmpeg)/lib"
+LIBS="$LIBS:$(echo ${DIR}/lib/jellyfin-ffmpeg)"
 exec ${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/lib/jellyfin-ffmpeg/ffprobe "$@"
