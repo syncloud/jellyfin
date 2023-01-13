77b6f7d93ec72c796bba20fa633f0f0d88f1df4f
Boris Rybalkin <ribalkin@gmail.com>
2023-01-13 13:34:19 +0000

    fix 602 error, switch to unix socket

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -2,25 +2,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-if [[ -z "$1" ]]; then
-    echo "usage $0 [start|stop|reload]"
-    exit 1
-fi
-
-case $1 in
-start)
-    timeout 100 /bin/bash -c 'until [ -S /var/snap/jellyfin/current/socket ]; do echo "waiting for socket"; sleep 1; done'
-	  /bin/rm -f /var/snap/jellyfin/common/web.socket
-    exec ${DIR}/nginx/sbin/nginx -c /snap/jellyfin/current/config/nginx.conf -p ${DIR}/nginx -g 'error_log stderr warn;'
-    ;;
-reload)
-    ${DIR}/nginx/sbin/nginx -c /snap/jellyfin/current/config/nginx.conf -s reload -p ${DIR}/nginx
-    ;;
-stop)
-    ${DIR}/nginx/sbin/nginx -c /snap/jellyfin/current/config/nginx.conf -s stop -p ${DIR}/nginx
-    ;;
-*)
-    echo "not valid command"
-    exit 1
-    ;;
-esac
+timeout 100 /bin/bash -c 'until [ -S /var/snap/jellyfin/current/socket ]; do echo "waiting for socket"; sleep 1; done'
+/bin/rm -f /var/snap/jellyfin/common/web.socket
+exec ${DIR}/nginx/sbin/nginx -c /snap/jellyfin/current/config/nginx.conf -p ${DIR}/nginx -e stderr
--- bin/service.server.sh
+++ bin/service.server.sh
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export JELLYFIN_kestrel__socket=true
 export JELLYFIN_kestrel__socketPath=/var/snap/jellyfin/current/socket
 
-$DIR/app/jellyfin/jellyfin --configdir /var/snap/jellyfin/current/config --datadir /var/snap/jellyfin/current/data --cachedir /var/snap/jellyfin/current/cache --ffmpeg /snap/jellyfin/current/bin/ffmpeg.sh
+exec $DIR/app/jellyfin/jellyfin --configdir /var/snap/jellyfin/current/config --datadir /var/snap/jellyfin/current/data --cachedir /var/snap/jellyfin/current/cache --ffmpeg /snap/jellyfin/current/bin/ffmpeg.sh
