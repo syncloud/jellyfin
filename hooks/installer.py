36efbcb3962fff5bf74585eb62df9e261779eb48
Boris Rybalkin <ribalkin@gmail.com>
2024-12-13 08:25:07 +0000

    10.10.3

--- hooks/installer.py
+++ hooks/installer.py
@@ -1,5 +1,6 @@
 import logging
 import shutil
+import time
 from os import path
 from os.path import join
 from subprocess import check_output, CalledProcessError
@@ -93,11 +94,25 @@ class Installer:
     def _install(self):
         self.log.info('configure install')
         app_storage_dir = storage.init_storage(APP_NAME, USER_NAME)
-        session = requests_unixsocket.Session()
-        wait_for_rest(session, "{0}/web/".format(SOCKET), 200, 10)
-        session.post("{0}/Startup/Complete".format(SOCKET))
+        self.complete()
+
         with open(self.install_file, 'w') as f:
             f.write('installed\n')
+
+    def complete(self):
+        session = requests_unixsocket.Session()
+        wait_for_rest(session, "{0}/web/".format(SOCKET), 200, 10)
+        attempt = 0
+        error = ''
+        while attempt < 20:
+            attempt += 1
+            response = session.post("{0}/Startup/Complete".format(SOCKET))
+            error = str(response.status_code) + ': ' + response.text
+            if response.status_code == 204:
+                return
+            time.sleep(10)
+
+        raise Exception(error)
 
     def on_domain_change(self):
         self.refresh_config()
--- test/lib.py
+++ test/lib.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 def login(selenium, device_user, device_password):
     selenium.open_app()
     selenium.screenshot('index')
+    # selenium.click_by(By.XPATH, '//span[.="Next"]')
     selenium.wait_or_screenshot(EC.element_to_be_clickable((By.CSS_SELECTOR, "#txtManualName")))
     selenium.find_by_id("txtManualName").send_keys(device_user)
     password = selenium.find_by_id("txtManualPassword")
