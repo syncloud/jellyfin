1f07b373dd5c2f94acd6db934b0750607ffcafca
Boris Rybalkin <ribalkin@gmail.com>
2026-01-04 22:43:50 +0000

    wait upgrade

--- test/upgrade.py
+++ test/upgrade.py
@@ -33,5 +33,5 @@ def test_upgrade(device, device_user, device_password, device_host, app_archive_
     device.run_ssh('snap install {0}'.format(app), retries=10)
     wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
     local_install(device_host, device_password, app_archive_path)
-    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 10)
+    wait_for_rest(requests.session(), "https://{0}".format(app_domain), 200, 20)
 
